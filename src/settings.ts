b351077cf9bb0053e2cb50bdfae4a72c8a38d214
Manu Erwin <manuerwin@gmail.com>
2021-11-06 22:03:01 +1300

    FEAT: nice exchange icon, always preserving metadata

--- src/settings.ts
+++ src/settings.ts
@@ -5,20 +5,10 @@ export namespace settings {
   export async function register() {
     await joplin.settings.registerSection("replaceResourcesSection", {
       label: "Replace Resources",
-      iconName: "fas fa-layer-group",
+      iconName: "fas fa-exchange-alt",
     });
 
     await joplin.settings.registerSettings({
-      preserveMetadataCreatedUpdated: {
-        value: false,
-        type: SettingItemType.Bool,
-        section: "replaceResourcesSection",
-        public: true,
-        label: "Preserve Resource Created and Updated",
-        description:
-          "Preserve the original resource Created and Updated values in the newly created resources.",
-      },
-
       filesPath: {
         value: "",
         type: SettingItemType.String,
