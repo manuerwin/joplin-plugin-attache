d35f76759e823d76b331af387f99e1496281a320
Manu Erwin <manuerwin@gmail.com>
2021-12-01 21:04:48 +1300

    DOCS: clarifying creation of files path

--- src/settings.ts
+++ src/settings.ts
@@ -15,7 +15,7 @@ export namespace settings {
         section: "replaceResourcesSection",
         public: true,
         label: "Files Path",
-        description: "Path to files that will replace your resources. Restart Joplin after setting for automatic creation.",
+        description: "Path to files that will replace your resources. Restarting Joplin will create this path if doesn't exist.",
       },
     });
     
