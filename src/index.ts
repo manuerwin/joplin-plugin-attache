7b570c1ec3c127c9e71c62c08e0ec32c6491382b
Manu Erwin <manuerwin@gmail.com>
2024-01-03 15:58:31 +1300

    chore: formatting

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,10 @@ joplin.plugins.register({
 	onStart: async function () {
 		console.log("onStart Attaché plugin");
 		await registerSettings();
-
 		await registerCommand();
-
 		await createMenuItems();
-
 		await init();
-
 		await onSyncCompleteEvent();
-
 		await syncConfiguredAndRunOnStart();
 	},
 });
